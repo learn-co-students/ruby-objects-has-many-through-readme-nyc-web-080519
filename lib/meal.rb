59d0ca854e295e4b2a320fabc111cdc278313abe
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-10 16:06:53 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
 class Meal
+  require_relative './customer.rb'
+  require_relative './waiter.rb'
+
+  attr_accessor :all, :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end  # ends initialize method
+
+  def self.all
+    @@all
+  end  # ends self.all method
+
+  
+
 
 end
